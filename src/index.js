d9cbce1dcca4fdb8a7a63de3e5e476b6908fd369
Valdo <valdo@Valdos-MBP.home>
2023-03-07 19:46:26 +0000

    hotfix: file system lib importation

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
-const fs = require("node:fs");
+const fs = require("fs");
 const path = require("node:path");
 const { Client, Collection, Events, GatewayIntentBits } = require("discord.js");
 
