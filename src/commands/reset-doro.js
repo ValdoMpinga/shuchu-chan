2140cc14b06b17cf6ba5337f870ae3305648a574
Valdo Mpinga <valdompinga2@gmail.com>
2023-11-19 10:59:25 +0000

    reset pomodoro confirmation done

--- src/commands/reset-doro.js
+++ src/commands/reset-doro.js
@@ -1,43 +1,41 @@
 const { SlashCommandBuilder } = require('discord.js');
-const { pomodoroActivityDetails, POMODORO_RESET_CODES } = require('../globals/pomodoroGlobals')
-const pomodoroReseter = require('../helpers/pomodoroHelpers/pomodoroReseter')
+const { pomodoroActivityDetails, POMODORO_RESET_CODES } = require('../globals/pomodoroGlobals');
+const pomodoroReseter = require('../helpers/pomodoroHelpers/pomodoroReseter');
 
-
-try
-{
-    module.exports = {
-        data: new SlashCommandBuilder()
-            .setName('reset-doro')
-            .setDescription('Resets the pomodoro')
-            .addIntegerOption(option =>
-                option.setName('confirmation')
-                    .setRequired(true)
-                    .addChoices(
-                        { name: 'No', value: 0 },
-                        { name: 'Yes', value: 1 }
-                    )
-            )
-        ,
-        async execute(interaction)
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('reset-doro')
+        .setDescription('Resets the pomodoro timer')
+        .addIntegerOption(option =>
+            option.setName('confirmation')
+                .setDescription('Confirms the intent to reset the timer, it is set to no in default to avoid mistakes')
+                .setRequired(true)
+                .addChoices(
+                    { name: 'No', value: 0 },
+                    { name: 'Yes', value: 1 }
+                )
+        ),
+    async execute(interaction)
+    {
+        try
         {
             if (pomodoroActivityDetails.isPomodoroActive)
             {
-                let selectedOption = interaction.options.getInteger('confirmation')
+                let selectedOption = interaction.options.getInteger('confirmation');
 
                 if (selectedOption == POMODORO_RESET_CODES.YES)
                 {
-                    pomodoroReseter()
-
+                    pomodoroReseter();
                     clearInterval(pomodoroActivityDetails.inactivityAlarmTimerId);
-
-                    await interaction.reply("Pomodoro has been reset!")
-                } 
-
+                    await interaction.reply("Pomodoro has been reset!");
+                }
             } else
-                await interaction.reply("You cant reset a pomodoro that isnt currently active.")
-        },
-    };
-} catch (e)
-{
-    console.log(e);
-}
+            {
+                await interaction.reply("You can't reset a pomodoro that isn't currently active.");
+            }
+        } catch (error)
+        {
+            console.error(error);
+        }
+    }
+};
