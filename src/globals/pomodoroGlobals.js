114e066c5e533f281d21b2957a9f4155119e637a
Valdo Mpinga <valdompinga2@gmail.com>
2023-11-19 09:56:13 +0000

    checkpoint

--- src/globals/pomodoroGlobals.js
+++ src/globals/pomodoroGlobals.js
@@ -3,7 +3,7 @@ const { Client, GatewayIntentBits } = require("discord.js");
 
 const CLIENT = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-const ALLOWED_CHANNELS = 'group-pomodoro'
+const ALLOWED_CHANNELS = 'pomodoro'
 
 const POMODORO_TIMING_DETAILS = {
     WORK_DURATION: ms('25m'),
